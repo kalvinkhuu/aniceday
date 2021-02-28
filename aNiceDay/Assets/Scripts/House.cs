c295f8185e70419d87407b13ed655b6879cd3247
npw22 <nickwilby@gmx.com>
2021-02-28 10:26:54 -0500

    House Fixed And Tree

---
+++ aNiceDay/Assets/Scripts/House.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    GameObject FrontOfHouse;
+    GameObject DoorFrame;
+
+    [SerializeField]
+    private float HouseZ = -2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FrontOfHouse = GameObject.Find("FrontOfHouse");
+        DoorFrame = GameObject.Find("DoorFrame");
+    }
+
+    public void SetDoorOpened(bool bOpened)
+    {
+        FrontOfHouse.SetActive(!bOpened);
+        DoorFrame.SetActive(!bOpened);
+    }
+
+    public float GetPlaceToStandInHouse()
+    {
+        return HouseZ;
+    }
+}
+
